a71535448a5364022f837b3d6831d891c804b47d
huuthanh <117473361+dhthanh1307@users.noreply.github.com>
2024-11-17 22:15:22 +0700

    fe-dashboard

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "../pages/Authen/Login";
+import Dashboard from "../pages/Dashboard";
 import {paths} from "./path"
 import Register from "../pages/Authen/Register";
 
@@ -7,7 +8,7 @@ export default function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path={paths.LOGIN} element={<Login />} />
         <Route path={paths.REGISTER} element={<Register />} />
       </Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ export default {
   ],
   theme: {
     extend: {
+      
+      colors: {
+        'primary': '#2e808a',
+        'secondary': '#c1654d',
+      },
+      textColor: {
+        'gradient': 'transparent',
+      },
+      backgroundImage: {
+        'gradient-text': 'linear-gradient(to right, var(--primary-color), var(--secondary-color))',
+      },
       boxShadow: {
         'gradient': '0px 4px 15px var(--primary-color), 0px 0px 30px var(--secondary-color)',
       },
