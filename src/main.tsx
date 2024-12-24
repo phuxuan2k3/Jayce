67fbeed9589d9d2b081ea0504833e427d729fc25
21120079 <hungtruonghl11@gmail.com>
2024-12-24 08:12:25 +0700

    test workflows

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./assets/styles/index.css";
 import App from "./App.tsx";
 import { createTheme, StyledEngineProvider, ThemeProvider } from "@mui/material";
 
-// Test
+// Test again
 
 const rootElement = document.getElementById("root");
 const theme = createTheme({
