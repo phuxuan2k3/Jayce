48783f25eabfa8752ed34b0ff449b2639d5f6b2a
quocanlikebanana <quocankg2611@gmail.com>
2025-05-04 16:52:31 +0700

    build

--- src/pages/manager/tests/index/ManagerTestsPage.tsx
+++ src/pages/manager/tests/index/ManagerTestsPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useDeleteManagerTestsByTestIdMutation, useGetManagerTestsQuery } from "../../../../features/tests/api/test.api-gen";
