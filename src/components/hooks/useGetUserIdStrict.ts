3e687669d404aee5e3aa5a0fa7725fc962f0a85c
quocanlikebanana <quocankg2611@gmail.com>
2025-06-27 19:15:41 +0700

    before-delete scenario

--- src/components/hooks/useGetUserIdStrict.ts
+++
@@ -1,8 +0,0 @@
-import { useAppSelector } from "../../app/hooks";
-import { authSelectors } from "../../features/auth/store/authSlice";
-
-
-export default function useGetUserIdStrict() {
-	const userId = useAppSelector(authSelectors.selectUserIdStrict);
-	return userId;
-}
